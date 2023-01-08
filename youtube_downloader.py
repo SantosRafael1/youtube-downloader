422020e056de6fa7db025a84eb775760a53c74f4
Rafael Santos <contactrafaelsantos@gmail.com>
2023-01-07 21:41:57 -0300

    Check if the url is valid or not

--- youtube_downloader.py
+++ youtube_downloader.py
@@ -1,6 +1,20 @@
 import youtube_dl
+from urllib.parse import urlparse
 
 url = str(input("URL: "))
+
+#check if URL is valid or not
+def valid_url(is_valid):
+    provided_url = urlparse(url)
+    if provided_url.netloc == "www.youtube.com" or provided_url.netloc == "youtu.be":
+        return True
+    else:
+        return False
+
+while valid_url(url) == False:
+    print("Invalid URL. Please provide a valid URL")
+    url = str(input("URL: "))
+
 
 ydl_opts = {
     'format': 'bestaudio/best',
