684c1fb2e8ef52f6f09f6b4b8d024bf702ca0465
Rafael Santos <contactrafaelsantos@gmail.com>
2023-01-13 19:37:27 -0300

    method to download audio implemented

--- GUI-version/youtube_downloader.py
+++ GUI-version/youtube_downloader.py
@@ -1,4 +1,6 @@
 import tkinter as tk
+import youtube_dl
+from urllib.parse import urlparse
 
 root = tk.Tk()
 
@@ -6,7 +8,29 @@ root.geometry('350x300')
 root.title("YouTube Downloader")
 root.resizable(False, False)
 
+var = tk.StringVar()
+
 top_label = tk.Label(root, text="Enter URL:").pack()
-url = tk.Entry(root, width=40).pack()
+url = tk.Entry(root, width=40, textvariable=var).pack()
+
+def download():
+    url_text = var.get()
+    options = {
+    'format': 'bestaudio/best',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '200',
+    }],
+    #specify your own path if you want
+    'outtmpl': 'songs/%(title)s.%(ext)s'
+    }
+
+    with youtube_dl.YoutubeDL(options) as ydl:
+        ydl.download([url_text])
+
+    
+
+download_button = tk.Button(root, text="Download MP3", command=download).pack()
 
 root.mainloop()
