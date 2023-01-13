777c915a7e3b77e57e7d25224fc137eb64627023
Rafael Santos <contactrafaelsantos@gmail.com>
2023-01-13 19:15:04 -0300

    Disable rezisable options and creating the text entry for url

--- GUI-version/youtube_downloader.py
+++ GUI-version/youtube_downloader.py
@@ -4,5 +4,9 @@ root = tk.Tk()
 
 root.geometry('350x300')
 root.title("YouTube Downloader")
+root.resizable(False, False)
+
+top_label = tk.Label(root, text="Enter URL:").pack()
+url = tk.Entry(root, width=40).pack()
 
 root.mainloop()
